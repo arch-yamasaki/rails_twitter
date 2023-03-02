82faf620964543a3792bd2da96ba5725e27086fb
arch-yamasaki <yohei7328@gmail.com>
2023-03-02 22:35:18 +0900

    修正

--- db/migrate/20230302091427_create_likes.rb
+++ db/migrate/20230302091427_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-
-      t.timestamps
+      t.references :user, presence: true
+      t.references :tweet, presence: true
     end
   end
 end
