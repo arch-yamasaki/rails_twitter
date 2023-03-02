d7fa112514f74844f66f82f98017164cab943ee7
arch-yamasaki <yohei7328@gmail.com>
2023-03-02 23:53:02 +0900

    specで作成されるもの調整

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework   :rspec, fixture: false
+      g.view_specs       false
+      g.controller_specs false
+    end
   end
 end
