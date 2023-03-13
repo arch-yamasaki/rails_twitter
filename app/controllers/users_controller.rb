8272e72f012f93ee885df5bfe27c336cfcc2be54
arch-yamasaki <yohei7328@gmail.com>
2023-03-13 16:00:29 +0900

    ログインなどできるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      # @user = User.find(params[:id])
+      redirect_to root_path
     end
 
     # Only allow a list of trusted parameters through.
