119e23c0b02e986329e7515b45c5aa58d18d4d6a
arch-yamasaki <yohei7328@gmail.com>
2023-03-16 12:20:30 +0900

    tweetの作成ができるように

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,10 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[ show edit update destroy ]
+  before_action :set_tweets, only: %i[ index destroy ]
 
   # GET /tweets
   def index
     @user = current_user
-    @tweets = Tweet.page(params[:page])
   end
 
   # GET /tweets/1
@@ -27,11 +27,8 @@ class TweetsController < ApplicationController
   # POST /tweets
   def create
     @tweet = Tweet.create(tweet_params)
-    @user = User.find(params[:user_id])
-    # binding.pry
     if @tweet.save
-      # redirect_to @tweet, notice: "Tweet was successfully created."
-      redirect_to tweet_path(@user, @tweet), notice: "Tweet was successfully updated."
+      redirect_to tweets_path, notice: "Tweet was successfully updated."
     else
       render :new, status: :unprocessable_entity
     end
@@ -58,8 +55,13 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find(paraams[:id])
     end
 
+    def set_tweets
+      @tweets = Tweet.page(params[:page])
+      # @tweets = Tweet.order("created_at DESC").page(params[:page])
+    end
+
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.require(:tweet).permit(:text).merge(user_id: params[:user_id])
+      params.require(:tweet).permit(:text).merge(user_id: current_user.id)
     end
 end
